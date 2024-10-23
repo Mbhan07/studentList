3999bdb4eca901ea23d6e78d9eb495085b08ae27
Mbhan07 <87451392+Mbhan07@users.noreply.github.com>
2024-10-23 10:11:47 -0700

    Add files via upload

---
+++ studentList.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <cstring>
+
+/*
+
+  This is the project "Student List," which allows you to add a student to a "database" (or vector),
+  delete the student from the vector, and output all students currently in the vetor.
+
+  Concepts of interest:
+
+  --> Using structs and pointers
+
+  --> Using vectors
+
+     --> Using iterators for vectors, as regular for loops are not appropiate here
+
+  --> Input/Output Stream
+
+      --> Entering a prompt in the command line (via keyboard) to have output in the command line (via display)
+
+                      / \
+                     /   \ 
+                    /     \
+		   /       \ 
+                  /         \
+		  /
+
+
+
+  Programmed by: Mahika Bhan, 10/25/2024
+
+*/  
+
+using namespace std;
+
+int main(){
+
+  return 0;
+}
+
